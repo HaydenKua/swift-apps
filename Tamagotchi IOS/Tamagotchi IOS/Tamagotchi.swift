5b99078553eaf219e387165b84f36280dd547d62
HaydenKua <kua.h@etoncollege.org.uk>
2020-01-16 13:17:25 +0000

    Doing tasks

--- Tamagotchi IOS/Tamagotchi IOS/Tamagotchi.swift
+++ Tamagotchi IOS/Tamagotchi IOS/Tamagotchi.swift
@@ -8,22 +8,64 @@
 
 import Foundation
 
-struct Tamagotchi {
-    var name: String
-    var gender: String
-    var hunger: Int
-    var happiness: Int
-    var age: Int
-    var weight: Double
+class Tamagotchi {
     
-    func displayAttributes() -> String {
+    private var name: String
+    private var gender: String
+    private var age: Int
+    private var hungry: Bool
+    private var happy: Int
+    private var healthy: Int
+    
+    init(name: String, gender: String, age: Int, hungry: Bool, happy: Int, healthy: Int) {
+        
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.hungry = hungry
+        self.happy = happy
+        self.healthy = healthy
+    }
+    
+    func displayStats() -> String{
         return """
         Name: \(name)
         Gender: \(gender)
-        Hunger: \(hunger)
-        Happiness: \(happiness)
         Age: \(age)
-        Weight: \(weight)
+        Hungry?: \(hungry)
+        Happy?: \(happy)
+        Healthy?: \(healthy)
         """
     }
+
+    func feedMeals() {
+        hungry = false
+        if happy != 10 {
+            happy += 1
+        }
+    }
+    
+    func feedSnack() {
+        hungry = false
+        if healthy != 0{
+            healthy -= 1
+        }
+        if happy != 10 {
+            happy += 1
+        }
+    }
+    
+    func playGame() {
+        if happy != 10 {
+            happy += 1
+        }
+    }
+    
+    func aging() {
+        if age != 10 {
+            age += 1
+        }
+    }
+
 }
+
