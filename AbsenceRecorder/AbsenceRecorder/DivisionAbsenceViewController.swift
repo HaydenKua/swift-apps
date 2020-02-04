092d4a9ce8751d2d4ea8c4441e3a6db2c1677018
Hayden Kua <kua.h@etoncollege.org.uk>
2020-02-04 08:23:57 +0000

    Absence Report

---
+++ AbsenceRecorder/AbsenceRecorder/DivisionAbsenceViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DivisionAbsenceViewControllerTableViewController.swift
+//  AbsenceRecorder
+//
+//  Created by Hayden Kua on 03/02/2020.
+//  Copyright © 2020 Hayden Kua. All rights reserved.
+//
+
+import UIKit
+
+class DivisionAbsenceViewController: UITableViewController {
+
+    var division: Division?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return division?.students.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Student", for: indexPath)
+        cell.textLabel?.text = division?.students[indexPath.row].surname
+        
+        return cell
+    }
+
+}
