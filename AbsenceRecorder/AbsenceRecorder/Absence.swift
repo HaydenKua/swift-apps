bd245798895779462fad9e4f34e4caebbc43ae6d
Hayden Kua <kua.h@etoncollege.org.uk>
2020-02-06 22:45:46 +0000

    Video lesson 3

---
+++ AbsenceRecorder/AbsenceRecorder/Absence.swift
@@ -0,0 +1,9 @@
+//
+//  Absence.swift
+//  AbsenceRecorder
+//
+//  Created by Hayden Kua on 06/02/2020.
+//  Copyright © 2020 Hayden Kua. All rights reserved.
+//
+
+import Foundation
---
+++ AbsenceRecorder/AbsenceRecorder/StudentCell.swift
@@ -0,0 +1,20 @@
+//
+//  StudentCellTableViewCell.swift
+//  AbsenceRecorder
+//
+//  Created by Hayden Kua on 06/02/2020.
+//  Copyright © 2020 Hayden Kua. All rights reserved.
+//
+
+import UIKit
+
+class StudentCellTableViewCell: UITableViewCell {
+
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+       
+    }
+
+}
