3ca3cba99410de99ab3dccf436b1025aff54be05
Hayden Kua <kua.h@etoncollege.org.uk>
2020-03-04 22:35:39 +0000

    Music By Location 3

--- MusicByLocation/MusicByLocation/ViewController.swift
+++ MusicByLocation/MusicByLocation/ViewController.swift
@@ -9,83 +9,42 @@
 import UIKit
 import CoreLocation
 
+/*
+ - Outlet
+ - Action
+ - Updating the display (using the outlet)
+ - Tweaking response to get artist names
+ 
+ */
+
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     let locationManager = CLLocationManager()
-    let geocoder = CLGeocoder()
+    let iTunesAdaptor = ITunesAdaptor()
+    let locationManagerDelegate = LocationManagerDelegate()
     
     @IBOutlet var musicRecommendations: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.requestWhenInUseAuthorization()
-        locationManager.delegate = self
+        locationManager.delegate = locationManagerDelegate
+        locationManagerDelegate.vc = self
     }
 
     @IBAction func findMusic(_ sender: Any) {
         locationManager.requestLocation()
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let firstLocation = locations.first {
-            geocoder.reverseGeocodeLocation(firstLocation, completionHandler: { (placemarks, error) in
-                if error != nil {
-                    self.musicRecommendations.text = "Could not perform lookup of location for latitude: \(firstLocation.coordinate.latitude.description)"
-                } else {
-                    if let firstPlacemark = placemarks?[0] {
-                        self.updateRecommendedArtists(search: firstPlacemark.locality)
-                    }
-                }
-            })
-            musicRecommendations.text = firstLocation.coordinate.latitude.description
+    func updateDisplay(text: String?) {
+        musicRecommendations.text = text
+    }
+    
+    func updateArtistsByLocation(text: String?) {
+        iTunesAdaptor.getArtists(search: text) { (artists) in
+            let names = artists?.map { return $0.artistName }
+            self.musicRecommendations.text = names?.joined(separator: ", ")
         }
     }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        musicRecommendations.text = "Could not access user's location. Error: \(error.localizedDescription)"
-    }
-    
-    func updateRecommendedArtists(search: String?) {
-        let searchTerm = search?.components(separatedBy: " ").first ?? "Lionel"
-        
-        guard let url = URL(string: "https://itunes.apple.com/search?term=\(searchTerm)&entity=musicArtist")
-            else{
-                print("Invalid URL. Not able to update recommended artists")
-                return
-        }
-        
-        let request = URLRequest(url: url)
-    
-        
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let data = data {
-                if let response = self.parseJson(json: data) {
-                    let names = response.results.map {
-                        return $0.artistName
-                    }
-                    
-                    DispatchQueue.main.async {
-                        self.musicRecommendations.text = names.joined(separator: ", ")
-                    }
-
-                }
-                
-            }
-        }.resume()
-        
-        
-    }
-    
-    func parseJson(json: Data) -> ArtistResponse? {
-        let decoder = JSONDecoder()
-        
-        if let artistResponse = try? decoder.decode(ArtistResponse.self, from: json) {
-            return artistResponse
-        } else {
-            print("Failed to decode to Artist Response")
-            return nil
-        }
-    }
-    
 }
 
